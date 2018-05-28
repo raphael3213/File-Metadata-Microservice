a5bcf77652341260259d3f080ae643b18b66b6b6
Glitch (ten-psychiatrist) <none>
2018-05-28 15:15:33 +0000

    🍱⏲ Checkpoint ./views/index.html:1174538/274 ./server.js:1174538/531

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-// server.js
-// where your node app starts
 
-// init project
 var express = require('express');
 var app = express();
+var multer=require('multer');
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+var upload=multer({dest:"upload/"})
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-// listen for requests :)
+app.post("/upload",upload.single('file'),function(req,res,next)
+         {
+  res.json(req.file);
+});
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
